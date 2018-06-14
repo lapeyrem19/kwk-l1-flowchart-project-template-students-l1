0d767fd41a666ab0c18defcff3892c8d8c096e53
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-14 14:51:23 +0000

    automatically backed up by learn

---
+++ app/models/movie.rb
@@ -0,0 +1,13 @@
+puts "What type of movie are you looking for?"
+
+def movie_age 
+puts "Adult or Children?"
+movie = gets.strip
+if movie = "Adult"
+  then puts "romance, drama, horror, fansty, documnetary, comedy, action/adventure, throback, sport, sad"
+end 
+if movie = "Children"
+  then puts "Animated or reallife"
+end 
+end
+
